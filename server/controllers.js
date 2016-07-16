c9c53bd43b9968a86c8dbb7f1b16344a909ba2e7
arc- <sp1re.exe@gmail.com>
2016-07-16 02:29:43 -0600

    progress

--- server/controllers.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    admin: require('./controllers/admin.js'),
-    adminLoginGet: require('./controllers/adminLogin.js').adminLoginGet,
-    adminLoginPost: require('./controllers/adminLogin.js').adminLoginPost,
-    index: require('./controllers/index.js'),
-    notFound: require('./controllers/notFound.js')
-};
