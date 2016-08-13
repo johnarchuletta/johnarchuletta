bc27122a79e7a4c8022fb4da8e503316118139e7
John Archuletta <sp1re.exe@gmail.com>
2016-08-13 04:14:22 -0600

    Minor changes

--- server/routes.js
+++ server/routes.js
@@ -16,65 +16,21 @@ var storage = multer.diskStorage({
 
 var upload = multer({storage: storage});
 
-//---------------------------------------------------------------------------------------------------------------------------------------
-// Route Definitions
-//---------------------------------------------------------------------------------------------------------------------------------------
-
 module.exports = function (server) {
-    //---------------------------------------------------------------------------------------------------------------------------------------
-    // GET
-    //---------------------------------------------------------------------------------------------------------------------------------------
-    
-    // Index
-    server.get('/',                         require('./controllers/index.js').get);
-    server.get('/admin',                    require('./controllers/admin.js').get);
-    server.get('/admin-login',              require('./controllers/admin-login.js').get);
-    
-    // Portfolio
-    server.get('/admin/portfolio',          require('./controllers/admin-portfolio.js').get);
-    
-    // Information
-    server.get('/admin/information',        require('./controllers/admin-information.js').get);
-    
-    //---------------------------------------------------------------------------------------------------------------------------------------
-    // POST
-    //---------------------------------------------------------------------------------------------------------------------------------------
-    
-    // Important to note about 'upload.array(name, limit)': the first argument *must* match the HTML file input's 'name' attribute!
-    
-    // Index
-    server.post('/admin-login',             require('./controllers/admin-login.js').post);
-    server.post('/admin/blog/create',
-                upload.single('image'),     require('./controllers/admin.js').post);
-    
-    // Portfolio
-    server.post('/admin/portfolio/create',
-                upload.array('images', 10), require('./controllers/admin-portfolio.js').post);
-    
-    //---------------------------------------------------------------------------------------------------------------------------------------
-    // PUT / UPDATE
-    //---------------------------------------------------------------------------------------------------------------------------------------
-    
-    // Index
-    server.post('/admin/blog/update',
-                upload.single('image'),     require('./controllers/admin.js').put);
-    
-    // Portfolio
-    server.post('/admin/portfolio/update',
-                upload.array('images', 10), require('./controllers/admin-portfolio.js').put);
-    
-    //---------------------------------------------------------------------------------------------------------------------------------------
-    // DELETE
-    //---------------------------------------------------------------------------------------------------------------------------------------
-    
-    // Index
-    server.get('/admin/blog/delete/:id',    require('./controllers/admin.js').delete);
-    
-    // Portfolio
+    server.get('/', require('./controllers/index.js').get);
+    server.get('/admin', require('./controllers/admin.js').get);
+    server.get('/admin-login', require('./controllers/admin-login.js').get);
+    server.get('/admin/portfolio', require('./controllers/admin-portfolio.js').get);
+    server.get('/admin/information', require('./controllers/admin-information.js').get);
+
+    server.post('/admin-login', require('./controllers/admin-login.js').post);
+    server.post('/admin/blog/create', upload.single('image'), require('./controllers/admin.js').post);
+    server.post('/admin/portfolio/create', upload.array('images', 10), require('./controllers/admin-portfolio.js').post);
+    server.post('/admin/blog/update', upload.single('image'), require('./controllers/admin.js').put);
+    server.post('/admin/portfolio/update', upload.array('images', 10), require('./controllers/admin-portfolio.js').put);
+
+    server.get('/admin/blog/delete/:id', require('./controllers/admin.js').delete);
     server.get('/admin/portfolio/delete/:id', require('./controllers/admin-portfolio.js').delete);
-    
-    //---------------------------------------------------------------------------------------------------------------------------------------
-    // 404 NOT FOUND
-    //---------------------------------------------------------------------------------------------------------------------------------------
-    server.get('*',                         require('./controllers/not-found.js').get);
-};
+
+    server.get('*', require('./controllers/not-found.js').get);
+}
