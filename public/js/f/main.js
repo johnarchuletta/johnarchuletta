4f63e5887241171193e6b2bef1937a4f98d0bc57
John Archuletta <sp1re.exe@gmail.com>
2016-09-16 06:15:23 -0600

    Working on 'terminal' design.

--- public/js/f/main.js
+++ public/js/f/main.js
@@ -1,30 +1,32 @@
 window.onload = function () {
-  // Find terminal input textbox.
+  // Store elements in variables.
   var terminalInput = document.getElementById('terminal-text-input');
   var window = document.querySelector('div.window');
   var titlebar = document.querySelector('div.window div.titlebar');
+
+  setTimeout(function() {
+    window.style.transform = 'scale(1)';
+  }, 1000);
 
   // TODO: Make the window draggable.
   titlebar.onmousedown = function (e) {
     console.log(e);
   }
 
-  window.onclick = function () {
-    terminalInput.focus();
-  }
-
   // Focus on terminal input textbox.
   terminalInput.focus();
 
-  // Do not let the user lose focus of terminal input.
-  terminalInput.onblur = function () {
+  // Focus on terminal command line when user clicks terminal window.
+  window.onclick = function () {
     terminalInput.focus();
-  };
+  }
 
   terminalInput.onkeypress = function (e) {
     if (e.key === 'Enter') {
       var input = this.value;
       this.value = '';
+
+      echoInput(input);
 
       switch (input) {
         case 'clear':
@@ -34,7 +36,7 @@ window.onload = function () {
           output(['Whoa there, buddy! Calm down!']);
           break;
         default:
-          output(['<span style="color: #E53F3A;">Command "' + input + '" not recognized.</span>']);
+          output(['<span style="color: #E53F3A;">No command \'' + input + '\' found.</span>']);
           break;
       }
     }
@@ -43,13 +45,13 @@ window.onload = function () {
   output([
     'Welcome to the personal website of <span style="color: #DFE300;">John Archuletta!</span>',
     '<span style="color: #707070;">Created 09-14-16 using NodeJS, ExpressJS, MongoDB, JavaScript, HTML, and Sass.</span>',
-    '<br>For a list of commands, please enter "cmdlist".<br><br>'
+    '<br>For a list of commands, please enter \'cmds\'.<br><br>'
   ]);
 }
 
 // Functions -------------------------------------------------------------------------------------------------------------------------------
 
-output = function (lines) {
+var output = function (lines) {
   var windowContent = document.querySelector('div.terminal-output');
   var newLine;
 
@@ -59,4 +61,10 @@ output = function (lines) {
 
     windowContent.appendChild(newLine);
   }
+}
+
+var echoInput = function(input) {
+  output([
+    '<span style="color: #71E53B;">user@linux</span><span style="color: #FFF;">:</span><span style="color: #7DD6E5">~</span><span style="color: #FFF;">$</span>&nbsp' + input,
+  ]);
 }
